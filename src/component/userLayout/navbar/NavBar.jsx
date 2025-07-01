058eeba4a140a549db744b089bb183e8c4e9691a
mr-harikrishnan <mr.harikrishnan85@gmail.com>
2025-07-01 15:31:15 +0530

    adding admin route link

--- src/component/userLayout/navbar/NavBar.jsx
+++ src/component/userLayout/navbar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -7,9 +8,9 @@ export default function NavBar() {
 
     return (
         <div className="">
-            <div className=' p-4  flex shadow-lg bg-white z-50'>
+            <div className=' p-4  flex shadow-lg bg-white z-50 items-center'>
 
-                <svg className="mx-3 w-8 lg:hidden"  fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
+                <svg className="mx-3 w-8 lg:hidden" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
 
@@ -21,6 +22,16 @@ export default function NavBar() {
                 <div className=' w-full flex justify-end items-center'>
                     <input className='w-42 border rounded-lg py-1 border-gray-300 ps-4' placeholder='search' type="text" />
                 </div>
+
+                <Link to={'/admin/dashboard'}>
+                    <div className='ml-6'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
+                        </svg>
+
+                    </div>
+                </Link>
+
 
 
                 <div className=' flex justify-center  ml-8 mt-1'>
