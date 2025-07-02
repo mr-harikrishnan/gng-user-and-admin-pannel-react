0b3ce642f055ebeb49d39489f483e7782f34f99d
mr-harikrishnan <mr.harikrishnan85@gmail.com>
2025-07-02 10:05:10 +0530

    adding toml file

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,6 @@ function App() {
 
           <Route path='/' element={<UserLayout></UserLayout>}>
             <Route path='veiwProduct/:id' element={<UserViewProduct></UserViewProduct>}></Route>
-    
           </Route>
 
 
--- src/component/userLayout/navbar/NavBar.jsx
+++ src/component/userLayout/navbar/NavBar.jsx
@@ -23,14 +23,14 @@ export default function NavBar() {
                     <input className='w-42 border rounded-lg py-1 border-gray-300 ps-4' placeholder='search' type="text" />
                 </div>
 
-                <Link to={'/admin/dashboard'}>
+                {/* <Link to={'/admin/dashboard'}>
                     <div className='ml-6'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
                         </svg>
 
                     </div>
-                </Link>
+                </Link> */}
 
 
 
